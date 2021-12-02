761dae9a41d6f22e2f87c5d45f7790235f0b2f32
Timothée DURAND <timothee.durand70@gmail.com>
2021-12-02 15:20:33 +0100

    add example view

--- api/src/index.php
+++ api/src/index.php
@@ -8,4 +8,4 @@ $database_connection = new \App\Database\Database();
 $manager = new \App\Manager\PostManager($database_connection->connection);
 $manager->getAllPosts();
 
-$manager->render("test", "tes", ["test" => "coucou"]);
+$manager->render("Article", "article", ["title" => "TEST"]);
