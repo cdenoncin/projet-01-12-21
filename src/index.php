73ca4a39f2925511a33c2722b2f41f7ebe3d29d9
Cécile Denoncin <ce.denoncin@gmail.com>
2021-12-03 11:29:24 +0100

    mega update views

--- src/index.php
+++ src/index.php
@@ -6,6 +6,9 @@ $database_connection = new \App\Database\Database();
 
 
 $manager = new \App\Manager\PostManager($database_connection->connection);
+
+$manager->getAll()[0];
+
 //$manager->create(["title"=>"coucou", "content"=> "conbnbnbucou", "thumbnail_url" => "https://source.unsplash.com/random", "author_id"=>"1", "publication_date"=>"2001-10-14 14:00:00", "id"=> "10"]);
 //$manager->create(["first_name" => "Timothée", "mail" =>"moi@gmail.com", "password"=>"coucou"]);
 //echo $manager->updatePost(["id"=>3, "content" => "baba au rhum", "publication_date"=>"2021-02-21 13:00:00"]);
@@ -14,5 +17,9 @@ $manager = new \App\Manager\PostManager($database_connection->connection);
 //$manager->update(["id"=> 1,"author_id"=>"1", "post_id"=>"1", "content"=>"dfdfdfdfdf"]);
 
 //$manager->delete(3);
-echo json_encode($manager->getAll(), JSON_PRETTY_PRINT);
-//$manager->render("Article", "article", ["title" => "TEST"]);
+// echo json_encode($manager->getAll(), JSON_PRETTY_PRINT);
+// $manager->render("Article", "article", ["title" => "TEST"]);
+
+
+$manager->render("Article", "article", ["articles" => $manager->getAll()[0]]);
+// $manager->render("Home Page", "homepage", ["articles" =>$manager->getAllPosts()]);
